16eb700a347588f18c010c6a4d8b741aba04e7e1
fangxiang <fangxiang@immotors.com>
2023-08-09 09:59:23 +0800

    feat: add

---
+++ springboot-002-first/src/main/java/com/bjpowernode/Springboot002FirstApplication.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot002FirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot002FirstApplication.class, args);
+    }
+
+}
---
+++ springboot-002-first/src/test/java/com/bjpowernode/Springboot002FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot002FirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
