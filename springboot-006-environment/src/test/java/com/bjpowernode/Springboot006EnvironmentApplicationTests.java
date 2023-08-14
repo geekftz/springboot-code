8b8d6814b5984da035c4b54b4b2fc38888c19fb0
hackftz <33424860+hackftz@users.noreply.github.com>
2023-08-14 20:22:27 +0800

    feat: add

---
+++ springboot-006-environment/src/test/java/com/bjpowernode/Springboot006EnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot006EnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
