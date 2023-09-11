18b7f30920e9120449fe22e1d3742f7f90e50ca5
hackftz <33424860+hackftz@users.noreply.github.com>
2023-09-11 21:19:25 +0800

    feat: add

--- springboot-020-restful/src/main/java/com/example/controller/MyRestController.java
+++ springboot-020-restful/src/main/java/com/example/controller/MyRestController.java
@@ -1,8 +1,6 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyRestController {
@@ -11,5 +9,21 @@ public class MyRestController {
     @GetMapping("/student/{stuId}")
     public String queryStudent(@PathVariable(value = "stuId") Integer stuId) {
         return "student id is -->" + stuId;
+    }
+
+
+    @PostMapping("/student/{name}/{age}")
+    public String createStudent(@PathVariable(value="name") String name,@PathVariable(value="age") Integer age) {
+        return "create student info -->" + name + age;
+    }
+
+    @PutMapping("/student/{id}/{age}")
+    public String modifyStudent(@PathVariable Integer id, @PathVariable Integer age) {
+        return "update resource, execute put request method" + id + age;
+    }
+
+    @DeleteMapping("/student/{id}")
+    public String removeStudentById(@PathVariable Integer id) {
+        return "remove student, id is: " + id;
     }
 }
