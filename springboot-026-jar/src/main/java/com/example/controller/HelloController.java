5d11d4f091083984eb20349ea9cbf932960c256b
hackftz <33424860+hackftz@users.noreply.github.com>
2023-10-09 23:00:16 +0800

    feat: add

--- springboot-026-jar/src/main/java/com/example/controller/HelloController.java
+++ springboot-026-jar/src/main/java/com/example/controller/HelloController.java
@@ -3,8 +3,10 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class HelloController {
+
     @RequestMapping("/hello")
     public ModelAndView hello(){
         ModelAndView mv  = new ModelAndView();
